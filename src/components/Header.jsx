42d2f84b40b7154b29fef46c66f9823560d673d5
McGyver105 <steve.titchener@hotmail.com>
2021-03-11 09:41:47 +0000

    adds a basic header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./components.css";
+
+const Header = () => {
+    return (
+        <header className="App__Header">
+            <p className="Header__Title">
+                Asset Safety Destination Finder
+                </p>
+        </header>
+    );
+};
+
+export default Header;
